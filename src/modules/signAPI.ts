e01df8e5652b241250b1006bec1961c0f018e5e4
jdy8739 <83811826+jdy8739@users.noreply.github.com>
2023-02-12 22:13:07 +0900

    Annot: 다음 작업 시 SignAPIManager 생성자에 axios interceptor 추가하도록 주석 추가

--- src/modules/signAPI.ts
+++ src/modules/signAPI.ts
@@ -16,6 +16,7 @@ class SignAPIManager implements ISignAPIManager {
 
   constructor(authCookieManagerParam: AuthCookieManager) {
     this.signInAxios = axios.create();
+    // TODO: add axios interceptor for showing toast UI.
     this.authCookieManager = authCookieManagerParam;
   }
 
